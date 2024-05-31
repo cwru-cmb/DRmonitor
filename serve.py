423b9517e219e60c3b1c3d886c937c95e90b79bd
Simon Silverstein <56710751+simonmso@users.noreply.github.com>
2024-05-31 10:34:00 -0400

    concatinate using pandas, takes forever

--- serve.py
+++ serve.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import re
+import pandas as pd
+import warnings
 
 # Usage example
 # $ python3 serve.py "Run 19 Logs"
@@ -15,24 +17,35 @@ def chldrn_labeled_with_date(dir_contents):
     # dir_contents: results of os.scandir(path)
     # returns the only directories labeled with ##-##-##
 
-    p = re.compile('^\d\d-\d\d-\d\d$')
+    p = re.compile(r'^\d\d-\d\d-\d\d$')
     return [ child for child in dir_contents if (p.match(child.name) and child.is_dir()) ]
 
 
 def concatenate_chnl_runs(dir_contents):
     # dir_contents: results of os.scandir(path)
-    # 
 
     dirs = chldrn_labeled_with_date(dir_contents)
+
+    channels = {}
 
     for dir in dirs:
         day = dir.name
         datafiles = os.scandir(dir.path)
         for df in datafiles:
-            print(df.name)
-            # WORKING ON: using pandas to concatinate the data
-            # We'll see if this uses too much memory and speed or is ok
+            # warn if the file is not named like the usual pattern
+            if (not df.name.endswith(f'{day}.log')):
+                warnings.warn(f'The file {df.path} is not of the form "[name] {day}.log" and will be ignored')
+            elif (df.name.startswith('Status_')):
+                print('ignoring status file for now')
+            else:
+                chnl_name = df.name[:-12]
+                data = pd.read_csv(df.path, header=None)
 
-
+                if chnl_name not in channels:
+                    channels[chnl_name] = data
+                else:
+                    channels[chnl_name] = pd.concat((channels[chnl_name], data))
+    
+    return channels
 
 concatenate_chnl_runs(contents)
